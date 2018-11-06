205e10e1be5e476dd57447591274660ece2a6f67
Demy <32939076+DemyDan@users.noreply.github.com>
2018-11-06 01:58:27 +0100

    ToLaptop

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour {
+
+    LightButton lightSwitch;
+
+    public Light flashLight;
+    public bool clickedE = false;
+	
+	// Update is called once per frame
+	void Update () {
+        FlashLight();
+	}
+
+    void FlashLight()
+    {
+        if(Input.GetKeyDown("q"))
+        {
+            flashLight.enabled = !flashLight.enabled;
+        }
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Checks if you are within the lightbutton range and enables you to press e
+        if (other.gameObject.tag.Equals("ligthSwitch"))
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                if (other.gameObject.GetComponent<LightButton>().spotLight.enabled == true)
+                {
+                    other.gameObject.GetComponent<LightButton>().spotLight.enabled = false;
+                    Debug.Log("Disabled");
+                }
+                else if (other.gameObject.GetComponent<LightButton>().spotLight.enabled == false)
+                {
+                    other.gameObject.GetComponent<LightButton>().spotLight.enabled = true;
+                    Debug.Log("Enabled");
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LightButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightButton : MonoBehaviour {
+
+    public Light spotLight;
+    public bool player;
+
+    private void OnGUI()
+    {
+        if(player)
+        {
+            GUI.Box(new Rect(140, Screen.height - 50, Screen.width - 300, 120), ("Press e to turn on"));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag.Equals("Player"))
+        {
+            player = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            player = false;
+        }
+    }
+
+}
